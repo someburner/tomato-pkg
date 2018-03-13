a9412657639a789d67cb38ebbd122a4f1ba7f3f4
Jeff H <jeffrey92@gmail.com>
2018-03-12 22:22:01 -0700

    updates

--- v1/src-rt-7.x.main/src/linux/sigs.sh
+++ v1/src-rt-7.x.main/src/linux/sigs.sh
@@ -1,5 +1,5 @@
 # sha256sum signatures for src-rt-7.x.main/src/linux
 
 declare -A SIGMAP=(
-["linux-2.6.36"]="533b57d6c56d7daba74801913f1365d9d612ac329022671be1bc37a14971bacf"
+["linux-2.6.36"]="5d3cdf4fba2b81430174b1dbed606de5c8555499cef96b055f989b3ab773ed11"
 );
